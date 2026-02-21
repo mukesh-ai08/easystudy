0122dc45a6f55c431d5e07353f332d96756e8073
Mukesh S <mukesh.ai25@stellamaryscoe.edu.in>
2026-02-21 15:55:44 +0530

    Update Firebase configuration with new credentials

--- js/firebase.js
+++ js/firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-aut
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MSG_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyDNT5bDtlPyoSBZvv_NFTmATIMLRIvpBLQ",
+  authDomain: "easystudy-96fa3.firebaseapp.com",
+  projectId: "easystudy-96fa3",
+  storageBucket: "easystudy-96fa3.appspot.com",
+  messagingSenderId: "222839424679",
+  appId: "1:222839424679:web:e25be32c8936b4d6643d82"
 };
 
 const app = initializeApp(firebaseConfig);
